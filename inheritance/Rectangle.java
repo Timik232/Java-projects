e559fd74c4bb8159f219fd904cb8e453ede6f6f3
Timur <100406268+Timik232@users.noreply.github.com>
2022-09-08 00:44:24 +0300

    part of third task

---
+++ inheritance/Rectangle.java
@@ -0,0 +1,55 @@
+package inheritance;
+
+public class Rectangle extends Shape{
+    protected double width;
+    protected double length;
+    public Rectangle(){
+    width = 1;
+    length = 1;
+    color = "white";
+    filled = false;
+    }
+    public Rectangle(double width, double length){
+        this.width = width;
+        this.length = length;
+        color = "white";
+        filled = false;
+    }
+    public Rectangle(double width, double length, String color, boolean filled){
+        this.width = width;
+        this.length = length;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return width*length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*width + 2*length;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape: rectangle," + "width=" + width + "length=" + length + "color= " + color;
+    }
+}
