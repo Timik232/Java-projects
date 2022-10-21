c4207856d571aca9eb6a95d32eedc7d3827ff672
Timur <100406268+Timik232@users.noreply.github.com>
2022-10-21 12:02:39 +0300

    11

---
+++ queue/TestClass.java
@@ -0,0 +1,28 @@
+package queue;
+
+public class TestClass {
+    public static void main(String[] args) {
+        ArrayQueueModule arr = new ArrayQueueModule(5);
+        if (arr.isEmpty())
+            System.out.println("empty");
+        arr.enqueue(5);
+        System.out.println(arr.size());
+        System.out.println(arr.element());
+        System.out.println(arr.dequeue());
+        if (arr.isEmpty())
+            System.out.println("empty");
+        for (int i = 0; i < 6; i++){
+            arr.enqueue(i);
+        }
+        System.out.println(arr);
+        for (int i = 0; i < 5; i++){
+            System.out.println(arr.dequeue());
+        }
+        if (arr.isEmpty())
+            System.out.println("empty");
+        String[] array = new String[2];
+        ArrayQueue strarr = new ArrayQueue(2,array);
+        strarr.enqueue("some");
+        System.out.println(strarr.dequeue());
+    }
+}
