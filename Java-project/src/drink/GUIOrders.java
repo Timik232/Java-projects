0e0ef2d22642c85cf1e7f99281fa0ec73aecdb99
Timur <100406268+Timik232@users.noreply.github.com>
2022-11-28 09:58:34 +0300

    nothing

--- Java-project/src/drink/GUIOrders.java
+++ Java-project/src/drink/GUIOrders.java
@@ -12,12 +12,44 @@ import java.util.Comparator;
 public class GUIOrders extends JFrame{ //gui for OrderManager
         private JFrame frame;
         private JPanel mainPanel;
+        private int depth;
         private final Color[] colors = {Color.CYAN, Color.lightGray, Color.GRAY};
         private final String[] columnNames = {"Добавить заказ", "Добавить интернет-заказ", "Удалить","Открыть"};
         public JPanel dishMenu(JPanel panel){
             panel.setLayout(new GridLayout());
 
             return panel;
+        }
+        public static  void GUIAddOrder(){
+            JFrame frame = new JFrame("Добавить заказ");
+            frame.setSize(300, 300);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            frame.setLayout(null);
+            frame.setLocationRelativeTo(null);
+            frame.setBackground(Color.DARK_GRAY);
+            JLabel label = new JLabel("Введите номер столика");
+            label.setBounds(10, 0, 200, 20);
+            JTextField textField = new JTextField("Введите номер столика");
+            frame.add(textField);
+            JButton button = new JButton("Добавить");
+            button.setLayout(null);
+            button.setBounds(70, 50, 75, 30);
+            final JTextField smallField = new JTextField(15);
+            smallField.setBounds(70, 20, 75, 30);
+            //mouselistener button
+            button.addActionListener(new java.awt.event.ActionListener() {
+                @Override
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    int number = Integer.parseInt(smallField.getText());
+                    TableOrdersManager tableOrdersManager = new TableOrdersManager();
+                    tableOrdersManager.add(new Order(), number);
+                    frame.dispose();
+                }
+            });
+            frame.add(smallField);
+            frame.add(button);
+            frame.add(label);
         }
         public GUIOrders(){
              /*frame = new JFrame("Restaurant");
@@ -27,8 +59,9 @@ public class GUIOrders extends JFrame{ //gui for OrderManager
             frame.setLayout(null);
             frame.setLocationRelativeTo(null);*/
              super("Restaurant");
-            JTabbedPane tabsLeft = new JTabbedPane(JTabbedPane.BOTTOM,
-                    JTabbedPane.SCROLL_TAB_LAYOUT);
+             depth = 0;
+            /*JTabbedPane tabsLeft = new JTabbedPane(JTabbedPane.BOTTOM,
+                   JTabbedPane.SCROLL_TAB_LAYOUT);*/
             /*for (int i = 0; i < 3; i++) {
                 JPanel panel = new JPanel();
                 panel.setBackground(colors[i]);
@@ -36,9 +69,34 @@ public class GUIOrders extends JFrame{ //gui for OrderManager
                 panel = dishMenu(panel);
                 tabsLeft.addTab(tabs[i],panel);
             }*/
-            getContentPane().setLayout(new GridLayout());
-            getContentPane().add(tabsLeft);
-            setSize(600,250);
+            JPanel panel = new JPanel();
+            panel.setLayout(new GridLayout(0,4,5,10));
+            getContentPane().setBackground(Color.lightGray);
+            for (int i = 0; i < 4; i++) {
+                JButton button = new JButton(columnNames[i]);
+                //button.setBackground(colors[0]);
+                button.addActionListener(e -> {
+                    if (e.getSource() == button) {
+                        if (button.getText().equals("Добавить заказ")) {
+                            //new GUIAddOrder();
+                            System.out.println(1);
+                        } else if (button.getText().equals("Добавить интернет-заказ")) {
+                            //new GUIAddInternetOrder();
+                            System.out.println(2);
+                        } else if (button.getText().equals("Удалить")) {
+                            //new GUIDeleteOrder();
+                            System.out.println(3);
+                        } else if (button.getText().equals("Открыть")) {
+                            //new GUIOpenOrder();
+                            System.out.println(4);
+                        }
+                    }
+                });
+                panel.add(button);
+            }
+            getContentPane().add(panel);
+            getContentPane().add
+            setSize(600,700);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setLocationRelativeTo(null);
         }
